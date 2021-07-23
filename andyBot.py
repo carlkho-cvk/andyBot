eba670fd668a03d001c12c9112a57b93f51a70f3
Carl Kho <62293444+carlkho-cvk@users.noreply.github.com>
2021-07-23 12:23:10 +0800

    Add files via upload
    Cleaned & commented on

--- andyBot.py
+++ andyBot.py
@@ -8,31 +8,51 @@ client = discord.Client()
 # placeChannelID Here
 channel_ID = 866334360542249021
 # placeBotToken here
-botToken = "ODY0NDE2NjMzMDE4Mzg0NDA0.YO1IuQ.eoV7yQZ0jfyKVEEVTILBfn7zysM"
+botToken = ""
 
 # variables for Hentai
 doujin = Hentai(321942)
 Hentai.exists(doujin.id)
 
 # variables for Discord bot
+
+
+# give instructions
+@client.event
+async def on_ready():
+    print("Bot on!")
+    general_channel = client.get_channel(channel_ID)
+    await general_channel.send('Place a 6-digit code to get the whole doujin')
+
+# when user places a 6-digit code
 @client.event
 async def on_message(message):
     if message.content.startswith('!hello'):
 
         general_channel = client.get_channel(channel_ID)
 
-        # step 1 - give details
-        embedVar = discord.Embed(title=doujin.title(Format.Pretty), description="yummy", color=0xFF33FF)
-        embedVar.add_field(name="tag", value=[tag.name for tag in doujin.tag], inline=False)
-        embedVar.add_field(name="upload date", value=doujin.upload_date, inline=False)
-        await message.channel.send(embed=embedVar)
+        # step 0 - ask user for code
+        if len(message.content) == 6:
+            await message.channel.send("Nice")
 
-        # step 2 - place all panels
-        for x in doujin.image_urls:
-            await general_channel.send(x)
+            code = message.content
 
-        # step 3 - reply to embed
-        await message.reply('Start reading from the top. Click the message I am replying to above me!', mention_author=False)
+            doujin = Hentai(code)
+            Hentai.exists(doujin.id)
+            await message.channel.send("You looked for: ")
+
+            # step 1 - give details
+            embedVar = discord.Embed(title=doujin.title(Format.Pretty), color=0xFF33FF)
+            embedVar.add_field(name="tag", value=[tag.name for tag in doujin.tag], inline=False)
+            embedVar.add_field(name="upload date", value=doujin.upload_date, inline=False)
+            await message.channel.send(embed=embedVar)
+
+            # step 2 - place all panels
+            for x in doujin.image_urls:
+                await general_channel.send(x)
+
+            # step 3 - reply to orig message (read from top)
+            await message.reply('Start reading from the top. Click the message I am replying to above me!', mention_author=False)
 
 #run bot on Discord
 client.run(botToken)
