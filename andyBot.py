3480dd64b8422c41e33963cc46fc4e08d6483cfe
Carl Kho <62293444+carlkho-cvk@users.noreply.github.com>
2021-07-23 12:25:40 +0800

    Update andyBot.py
    Final | Retrieves user inputs. Best practice = direct messages.

--- andyBot.py
+++ andyBot.py
@@ -27,32 +27,31 @@ async def on_ready():
 # when user places a 6-digit code
 @client.event
 async def on_message(message):
-    if message.content.startswith('!hello'):
 
-        general_channel = client.get_channel(channel_ID)
+    general_channel = client.get_channel(channel_ID)
 
-        # step 0 - ask user for code
-        if len(message.content) == 6:
-            await message.channel.send("Nice")
+    # step 0 - ask user for code
+    if len(message.content) == 6:
+        await message.channel.send("Nice")
 
-            code = message.content
+        code = message.content
 
-            doujin = Hentai(code)
-            Hentai.exists(doujin.id)
-            await message.channel.send("You looked for: ")
+        doujin = Hentai(code)
+        Hentai.exists(doujin.id)
+        await message.channel.send("You looked for: ")
 
-            # step 1 - give details
-            embedVar = discord.Embed(title=doujin.title(Format.Pretty), color=0xFF33FF)
-            embedVar.add_field(name="tag", value=[tag.name for tag in doujin.tag], inline=False)
-            embedVar.add_field(name="upload date", value=doujin.upload_date, inline=False)
-            await message.channel.send(embed=embedVar)
+        # step 1 - give details
+        embedVar = discord.Embed(title=doujin.title(Format.Pretty), color=0xFF33FF)
+        embedVar.add_field(name="tag", value=str([tag.name for tag in doujin.tag]), inline=False)
+        embedVar.add_field(name="upload date", value=str(doujin.upload_date), inline=False)
+        await message.channel.send(embed=embedVar)
 
-            # step 2 - place all panels
-            for x in doujin.image_urls:
-                await general_channel.send(x)
+        # step 2 - place all panels
+        for x in doujin.image_urls:
+            await general_channel.send(x)
 
-            # step 3 - reply to orig message (read from top)
-            await message.reply('Start reading from the top. Click the message I am replying to above me!', mention_author=False)
+        # step 3 - reply to orig message (read from top)
+        await message.reply('Start reading from the top. Click the message I am replying to above me!', mention_author=False)
 
 #run bot on Discord
 client.run(botToken)
